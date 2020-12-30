35c6e908c30626eef5e50c32420a089a121c4ac6
Carlos de Jesús Benoit Mendívil <cdjbm03@gmail.com>
2020-12-30 01:50:07 -0700

    [ADDED]: Landing Page structure

---
+++ admin-frontend/pages/Header.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { spacing } from '@material-ui/system';
+import { Box } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  toolbar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  toolbarSecondary: {
+    justifyContent: 'space-between',
+    overflowX: 'auto',
+  },
+  toolbarLink: {
+    padding: theme.spacing(1),
+    flexShrink: 0,
+  },
+}));
+
+export default function Header(props) {
+  const classes = useStyles();
+  const { sections, title } = props;
+
+  return (
+    <React.Fragment>
+      <Toolbar className={classes.toolbar}>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="start"
+          noWrap
+          className={classes.toolbarTitle}
+        >
+          {title}
+        </Typography>
+        <Box mx={1}>
+          <Button variant="outlined" color="inherit">
+            Registrarse
+          </Button>
+        </Box>
+        <Box mx={1}>
+          <Button variant="outlined" p={5} color="inherit">
+            Ingresar
+          </Button>
+        </Box>
+      </Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        className={classes.toolbarSecondary}
+      >
+        {sections.map(section => (
+          <Link
+            color="inherit"
+            noWrap
+            key={section.title}
+            variant="body2"
+            href={section.url}
+            className={classes.toolbarLink}
+          >
+            {section.title}
+          </Link>
+        ))}
+      </Toolbar>
+    </React.Fragment>
+  );
+}
+
+Header.propTypes = {
+  sections: PropTypes.array,
+  title: PropTypes.string,
+};
--- admin-frontend/pages/index.js
+++ admin-frontend/pages/index.js
@@ -1,12 +1,187 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
-const index = () => {
+import Header from './Header';
+
+const sections = [
+  { title: 'Tecnologia', url: '#' },
+  { title: 'Diseño', url: '#' },
+  { title: 'Negocios', url: '#' },
+  { title: 'Politica', url: '#' },
+  { title: 'VideoJuegos', url: '#' },
+  { title: 'Ciencia', url: '#' },
+  { title: 'Healthy', url: '#' },
+  { title: 'Moda', url: '#' },
+  { title: 'Viajes', url: '#' },
+];
+
+const footers = [
+  {
+    title: 'Compañia',
+    description: ['Equipo', 'Historia', 'Contactanos', 'Locaciones'],
+  },
+  {
+    title: 'Ayuda',
+    description: ['Centro de ayuda', 'FAQ'],
+  },
+  {
+    title: 'Legal',
+    description: ['Politica de privacidad', 'Terminos de uso'],
+  },
+];
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    borderTop: `2px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    backgroundColor: '#d6d6d6',
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Album() {
+  const classes = useStyles();
+
   return (
-    <div>
-      {' '}
-      <h1>Inicialización FrontEnd</h1>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Header title="GuaoBlogs" sections={sections} />
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="md">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              GuaoBlogs
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Crea un blog atractivo y adaptado a tu estilo. Dispones de un
+              surtido de plantillas para elegir, fáciles de usar, con diseños
+              flexibles y montones de imágenes de fondo, o puedes diseñar un
+              blog totalmente nuevo.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="outlined" size="large" color="primary">
+                    Crea tu blog
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="lg">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={3}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Titulo
+                    </Typography>
+                    <Typography>
+                      Resumen del blog: Lorem ipsum dolor sit amet consectetur
+                      adipisicing elit.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Leer más
+                    </Button>
+                    <Button size="small" color="primary">
+                      Ver mas tarde
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Container maxWidth="xl" component="footer" className={classes.footer}>
+        <Grid container spacing={4} justify="center">
+          {footers.map(footer => (
+            <Grid item xs={6} sm={2} key={footer.title}>
+              <Typography variant="h6" color="textPrimary" gutterBottom>
+                {footer.title}
+              </Typography>
+              <ul>
+                {footer.description.map(item => (
+                  <li key={item}>
+                    <Link href="#" variant="subtitle1" color="primary">
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      {/* End footer */}
+    </React.Fragment>
   );
-};
-
-export default index;
+}
